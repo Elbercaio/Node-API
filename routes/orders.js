3ab6c66e30231a5ed18ac8c4e0947e1deec2c1fd
Elber Lopes <elber.lopes@somosmesha.com>
2021-12-06 09:13:05 -0300

    mysql implementation

--- routes/orders.js
+++ routes/orders.js
@@ -1,35 +1,89 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("../mysql").pool;
 
 router.get("/", (req, res, next) => {
-  res.status(200).send({
-    message: "GET all Orders",
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "SELECT * FROM orders",
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(200).send({
+          response: result,
+        });
+      }
+    );
   });
 });
 
 router.get("/:order_id", (req, res, next) => {
-  const id = req.params.order_id;
-  res.status(200).send({
-    message: `GET Order: ${id}`,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "SELECT * FROM orders WHERE order_id=?",
+      req.params.product_id,
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(200).send({
+          response: result,
+        });
+      }
+    );
   });
 });
 
 router.post("/", (req, res, next) => {
-  const order = {
-    product_id: req.body.id,
-    quantity: req.body.quantity,
-  };
-  res.status(201).send({
-    message: `Order created`,
-    id: product_id,
-    order: order,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "INSERT INTO orders (order_id, quantity, product_id) VALUES (?, ?, ?)",
+      [req.body.id, req.body.quantity, res.body.product_id],
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(201).send({
+          message: `Order successfully inserted`,
+          id_product: result.insertId,
+        });
+      }
+    );
   });
 });
 
 router.delete("/:order_id", (req, res, next) => {
-  const id = req.params.order_id;
-  res.status(201).send({
-    message: `DELETE Product: ${id}`,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      `DELETE FROM orders WHERE order_id = ?`,
+      req.params.product_id,
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(202).send({
+          message: `Order successfully deleted`,
+          id_product: result.insertId,
+        });
+      }
+    );
   });
 });
 
--- routes/products.js
+++ routes/products.js
@@ -1,41 +1,114 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("../mysql").pool;
 
 router.get("/", (req, res, next) => {
-  res.status(200).send({
-    message: "GET all Products",
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "SELECT product_id, name, price FROM products",
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(200).send({
+          response: result,
+        });
+      }
+    );
   });
 });
 
 router.get("/:product_id", (req, res, next) => {
-  const id = req.params.product_id;
-  res.status(200).send({
-    message: `GET Product: ${id}`,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "SELECT product_id, name, price FROM products WHERE product_id=?",
+      req.params.product_id,
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(200).send({
+          response: result,
+        });
+      }
+    );
   });
 });
 
 router.post("/", (req, res, next) => {
-  const product = {
-    name: req.body.name,
-    price: req.body.price,
-  };
-  res.status(201).send({
-    message: `Product created`,
-    product: product,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "INSERT INTO products (name, price) VALUES (?, ?)",
+      [req.body.name, req.body.price],
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(201).send({
+          message: `Product successfully inserted`,
+          id_product: result.insertId,
+        });
+      }
+    );
   });
 });
 
 router.patch("/:product_id", (req, res, next) => {
-  const id = req.params.product_id;
-  res.status(201).send({
-    message: `PATCH Product: ${id}`,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      `UPDATE products
+        SET name = ?,
+            price = ?
+      WHERE product_id = ?`,
+      [req.body.name, req.body.price, req.params.product_id],
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(202).send({
+          message: `Product successfully updated`,
+          id_product: result.insertId,
+        });
+      }
+    );
   });
 });
 
 router.delete("/:product_id", (req, res, next) => {
-  const id = req.params.product_id;
-  res.status(201).send({
-    message: `DELETE Product: ${id}`,
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      `DELETE FROM products WHERE product_id = ?`,
+      req.params.product_id,
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        res.status(202).send({
+          message: `Product successfully deleted`,
+          id_product: result.insertId,
+        });
+      }
+    );
   });
 });
 
